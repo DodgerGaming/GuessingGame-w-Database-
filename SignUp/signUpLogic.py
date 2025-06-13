24efc998579ff28a7cd22a2d8d4db81af2a0ec22
Kenneth James Boncales <kennethboncales608@gmail.com>
2025-06-14 06:56:51 +0800

    added confirm account function

--- SignUp/signUpLogic.py
+++ SignUp/signUpLogic.py
@@ -1,7 +1,22 @@
 import customtkinter
+from tkinter import messagebox
+import sqlite3
 
-def confirm_account():
-    pass
+def confirm_account(username, password1, password2):
+    # validating if the user input something in text fields
+    if username == "" or password1 == "" or password2 == "":
+        messagebox.showerror(message = "Please input all text fields!")
+    else:
+        is_existing(username) 
+
+def is_existing(username): # checks if the inputted username is already existing in database
+    connect = sqlite3.connect("System.db")
+    
+    cursor = connect.execute("SELECT USERNAME FROM PLAYER WHERE USERNAME = ?", (username))
+
+    # TODO: Implementation of checking if the inputted name of user is already on database 
+    
+
 
 def create_account():
     pass
